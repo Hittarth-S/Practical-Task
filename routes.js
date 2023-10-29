8cde0d6b723b814b8e41ea4d70dca1d0f562978a
hvs911 <hittarth911@gmail.com>
2023-10-29 16:10:31 +0530

    Update: Error Page Added

--- routes.js
+++ routes.js
@@ -40,4 +40,16 @@ app.get("/shop", (request, response) => {
   });
 });
 
+/* ERROR PAGE  */
+app.get("/:id", (request, response) => {
+  const filePath = path.resolve(__dirname, "./build", "index.html");
+  fs.readFile(filePath, "utf8", function (err, data) {
+    if (err) {
+      return console.log(err);
+    }
+    let result = data;
+    response.send(result);
+  });
+});
+
 module.exports = app;
