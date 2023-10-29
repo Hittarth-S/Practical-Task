6687494870c2b44a0a11b29e40e99c89d18b238b
hvs911 <hittarth911@gmail.com>
2023-10-29 16:09:21 +0530

    Update: Added 404 Page

--- routes.js
+++ routes.js
@@ -64,4 +64,15 @@ app.get("/users", (request, response) => {
   });
 });
 
+/* ERROR PAGE  */
+app.get("/:id", (request, response) => {
+  const filePath = path.resolve(__dirname, "./build", "index.html");
+  fs.readFile(filePath, "utf8", function (err, data) {
+    if (err) {
+      return console.log(err);
+    }
+    let result = data;
+    response.send(result);
+  });
+});
 module.exports = app;
